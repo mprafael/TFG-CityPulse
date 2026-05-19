77556947d5356c122e44b595afad72acae36bf10
mprafael <maciasperafael@gmail>
2026-05-19 23:24:40 +0200

    Version 1.0: inicio de smtp

--- citypulse-backend/src/server.js
+++ citypulse-backend/src/server.js
@@ -25,13 +25,8 @@ const prisma = new PrismaClient({ adapter });
 
 // Email Service Initialization
 const transporter = nodemailer.createTransport({
-  host: '142.251.10.108',
-  port: 587,
-  secure: false,
-  auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
-  tls: {
-    rejectUnauthorized: false
-  }
+  service: 'gmail',
+  auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS }
 });
 
 transporter.verify().then(() => {
